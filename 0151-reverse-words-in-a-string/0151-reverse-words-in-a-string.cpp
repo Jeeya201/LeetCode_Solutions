5ea47444d8cd57825a04179a31ee3944b57529ff
Jeeya Shah <104685075+Jeeya201@users.noreply.github.com>
2023-10-05 23:15:36 +0530

    Time: 3 ms (86.87%), Space: 21.3 MB (13.97%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    
+    // YOU can say both solutions : 2nd one uses map , so extra space ; but 1st one uses .substr which is inbuilt ----1st preffered
+    
+    string reverseWords(string s) {
+        int n=s.size();
+        int i=0;
+        int j=0;
+        string res="";
+        while(i<n)
+        {
+            while(i<n && s[i]==' ')
+            {
+                i++;
+            }
+            if(i>=n) break;
+            j=i+1;
+            while(j<n && s[j]!=' ')
+            {
+                j++;
+            }
+            string sub=s.substr(i,j-i);
+            if(res.size()==0)
+            {
+                res+=sub;
+            }
+            else
+            {
+                res=sub+" "+res;
+            }
+            i=j+1;
+        }
+        return res;
+        
+    }
+    
+};
+
+// class Solution {
+// public:
+//     string reverseWords(string s) {
+//         unordered_map<int, string> mp;
+//         int n = s.length(), word_count = 1;
+//         for(int i=0; i<n; i++){
+//             if(s[i] != ' '){
+//                 string temp = "";
+//                 while(i<n && (s[i] != ' ' || i==n-1)){
+//                     temp += s[i++];
+//                 }
+//                 mp[word_count++] = temp;
+//             }
+//         }
+//         string ans = "";
+//         while(--word_count){
+//             ans += mp[word_count] + " ";
+//         }
+//         ans.pop_back();
+//         return ans;
+//     }
+// };
