5164b9da5d4ee132aa464c0fb6fbe0afa7886acb
Jeeya Shah <104685075+Jeeya201@users.noreply.github.com>
2024-05-31 16:15:09 +0530

    Time: 7 ms (36.50%), Space: 9.9 MB (53.01%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -2,38 +2,120 @@ class Solution {
 public:
     
     // YOU can say both solutions : 2nd one uses map , so extra space ; but 1st one uses .substr which is inbuilt ----1st preffered
-    
-    string reverseWords(string s) {
-        int n=s.size();
-        int i=0;
-        int j=0;
-        string res="";
-        while(i<n)
+        string reverseWords(string s) {
+    stack<string> st;
+
+    int i=0;
+    string word="";
+    while(i<s.size())
+    {
+        if(s[i]!=' ')
         {
-            while(i<n && s[i]==' ')
+            word+=s[i];
+            i++;
+        }
+        else
+        {
+            while(s[i]==' ')
             {
                 i++;
             }
-            if(i>=n) break;
-            j=i+1;
-            while(j<n && s[j]!=' ')
+            if(word!="") //to avoid starting space : Only if its not a space , then you add it to stack
             {
-                j++;
+                st.push(word);
+                // cout<<word;
+                word="";
             }
-            string sub=s.substr(i,j-i);
-            if(res.size()==0)
-            {
-                res+=sub;
-            }
-            else
-            {
-                res=sub+" "+res;
-            }
-            i=j+1;
         }
-        return res;
-        
     }
+            st.push(word);
+string ans;
+            cout<<endl;
+while(!st.empty())
+    {
+                word=st.top();
+                // cout<<"word "<<word;
+                st.pop();
+                if(st.size()==0)
+                {
+                    ans+=word;
+                }
+                else if(word!="")//To avoid leading or  trailing spaces 
+                {
+                    ans+=word+' '; 
+                }
+            }
+            return ans;
+        }    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+//     string reverseWords(string s) {
+//         int n=s.size();
+//         int i=0;
+//         int j=0;
+//         string res="";
+//         while(i<n)
+//         {
+//             while(i<n && s[i]==' ')
+//             {
+//                 i++;
+//             }
+//             if(i>=n) break;
+//             j=i+1;
+//             while(j<n && s[j]!=' ')
+//             {
+//                 j++;
+//             }
+//             string sub=s.substr(i,j-i);
+//             if(res.size()==0)
+//             {
+//                 res+=sub;
+//             }
+//             else
+//             {
+//                 res=sub+" "+res;
+//             }
+//             i=j+1;
+//         }
+//         return res;
+        
+//     }
     
 };
 
