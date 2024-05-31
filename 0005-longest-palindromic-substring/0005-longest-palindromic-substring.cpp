04466972c15f8a8e6f81110c1ee9b0a4c30446ba
Jeeya Shah <104685075+Jeeya201@users.noreply.github.com>
2024-05-31 18:06:41 +0530

    Time: 11 ms (83.74%), Space: 8.1 MB (93.35%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -7,49 +7,116 @@ public:
 // cause , inn even length string , you ca have odd length substring , eg ababas
 // so this  is correct solution
     string longestPalindrome(string s) {
+        
         int n=s.length();
-        int len=0;
-        int ans=1;
         int l=0;
         int r=0;
         int start=0;
-        
-        for(int i=0;i<n;i++){
+        int len=0;
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
             l=i;
-            r=i;            
+            r=i;
             while(l>=0 && r<n && s[l]==s[r])
             {
                 l--;
-                r++;   
+                r++;
             }
             len=r-l-1;
-            if(len>=ans)
+            if(ans<=len)
             {
                 ans=len;
                 start=l+1;
             }
-            // ans=max(ans,len);
-            // cout<<l;
         }
-         for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++)
+        {
             l=i;
-            r=i+1;            
+            r=i+1;
             while(l>=0 && r<n && s[l]==s[r])
             {
                 l--;
-                r++;   
+                r++;
             }
             len=r-l-1;
-            if(len>=ans)
+            if(ans<=len)
             {
                 ans=len;
                 start=l+1;
             }
-            // ans=max(ans,len);
-            // cout<<l;
         }
         
         return s.substr(start,ans);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//         int n=s.length();
+//         int len=0;
+//         int ans=1;// minimum palindrome can be of length 1 ; eg a,b,d,g,v,etc are all palindrome
+//         int l=0;
+//         int r=0;
+//         int start=0;
+        
+//         for(int i=0;i<n;i++){
+//             l=i;
+//             r=i;            
+//             while(l>=0 && r<n && s[l]==s[r])
+//             {
+//                 l--;
+//                 r++;   
+//             }
+//             len=r-l-1;
+//             if(len>=ans)
+//             {
+//                 ans=len;
+//                 start=l+1;
+//             }
+//             // ans=max(ans,len);
+//             // cout<<l;
+//         }
+        //  for(int i=0;i<n;i++){
+        //     l=i;
+        //     r=i+1;            
+        //     while(l>=0 && r<n && s[l]==s[r])
+        //     {
+        //         l--;
+        //         r++;   
+        //     }
+        //     len=r-l-1;
+        //     if(len>=ans)
+        //     {
+        //         ans=len;
+        //         start=l+1;
+        //     }
+        //     // ans=max(ans,len);
+        //     // cout<<l;
+        // }
+        
+        // return s.substr(start,ans);
     }
 };
 
